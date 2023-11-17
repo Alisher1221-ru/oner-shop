62f74642d1779fe4a44d328d0c329355a7ae5e2f
Alisher <123545442+Alisher1221-ru@users.noreply.github.com>
2023-11-17 13:48:39 +0500

    Add files via upload

--- one-shop/src/index.js
+++ one-shop/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import Headers from './components/headers/headers';
 import Footers from './components/footers/futers';
 import {ChakraProvider} from "@chakra-ui/react"
+import { BrowserRouter } from 'react-router-dom'
+import Main from './components/mains/main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <Footers />
-      <Headers />
-    </ChakraProvider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <ChakraProvider>
+        <Headers />
+        <Main />
+        <Footers />
+      </ChakraProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
